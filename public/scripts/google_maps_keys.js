ac0c77ad0f2f8c38dd5f5a468aad2379afb2b7cf
nreddy216 <nreddy216@gmail.com>
2016-04-13 12:29:24 -0700

    removed api key from google since unneeded

--- public/scripts/google_maps_keys.js
+++ public/scripts/google_maps_keys.js
@@ -1,1 +1,3 @@
-var google_maps_keys ="AIzaSyAvM_uaLeeaqnLUKfqwbrpkPhRPZtC1rlc"
+GOOGLE_PLACES_API_KEY ="AIzaSyD6rkQVQbh5mEHYFKhrW5yrDEE8aHh_rkw"
+
+GOOGLE_PLACES_ENDPOINT = "https://maps.googleapis.com/maps/api/js?" + GOOGLE_PLACES_API_KEY + "&libraries=places"
