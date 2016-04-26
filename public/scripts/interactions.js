ff23c1e5088213d72cd4efacce757e835c5432cd
nreddy216 <nreddy216@gmail.com>
2016-04-26 14:57:58 -0700

    change to materialize and fix navbar

---
+++ public/scripts/interactions.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  console.log("Interactions JS linked");
+
+  //collapse navbar materialize mobile
+  $(".button-collapse").sideNav();
+
+  //dropdown navbar
+  $(".dropdown-button").dropdown();
+
+
+
+});
